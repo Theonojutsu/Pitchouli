0adf188c00a6291c6a41ced7237e6283c1b5b36a
PichonMalte <alexandrechahinian@gmail.com>
2023-07-08 14:47:14 +0200

    DéplacementCaméra

---
+++ Assets/Pichon/Déplacement.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class Déplacement : MonoBehaviour
+{
+    public float speed = 5f; // Vitesse de déplacement de l'objet
+
+    private void Update()
+    {
+        // Calculer le déplacement en fonction de la vitesse et du temps écoulé depuis la dernière frame
+        float moveAmount = speed * Time.deltaTime;
+
+        // Déplacer l'objet sur l'axe x
+        transform.Translate(Vector2.right * moveAmount);
+    }
+}
