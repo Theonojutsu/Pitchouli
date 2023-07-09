98ee51418e8ca0a60b62f6451733154322fb35ed
PichonMalte <alexandrechahinian@gmail.com>
2023-07-09 19:01:20 +0200

    Fini

--- Assets/Theo/script/Lose.cs
+++ Assets/Theo/script/Lose.cs
@@ -3,32 +3,13 @@ using UnityEngine.SceneManagement;
 
 public class Lose : MonoBehaviour
 {
-    private Camera mainCamera;
-    private float limiteGauche;
-
-    void Start()
+    private void OnBecameInvisible()
     {
-        mainCamera = Camera.main;
-
-    }
-
-    void Update()
-    {
-        limiteGauche = mainCamera.ScreenToWorldPoint(new Vector3(0, 0, 0)).x;
-
-        if (transform.position.x <= limiteGauche)
-        {
-            SceneManager.LoadScene(0);
-            
-        }
-    }
-
-    void OnTriggerEnter2D(Collider2D collision)
-    {
-        if ((collision.gameObject.tag == "Spike"))
-        {
-            SceneManager.LoadScene(0);
-        }
+        SceneManager.LoadScene(1);
     }
 }
 
+        
+    
+            
+        
