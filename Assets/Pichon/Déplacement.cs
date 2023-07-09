df6797048e54a7112dec130ae817976560c1fa12
PichonMalte <alexandrechahinian@gmail.com>
2023-07-09 14:35:52 +0200

    Pich

--- Assets/Pichon/Déplacement.cs
+++ Assets/Pichon/Déplacement.cs
@@ -8,7 +8,7 @@ public class Déplacement : MonoBehaviour
     {
         // Calculer le déplacement en fonction de la vitesse et du temps écoulé depuis la dernière frame
         float moveAmount = speed * Time.deltaTime;
-
+         
         // Déplacer l'objet sur l'axe x
         transform.Translate(Vector2.right * moveAmount);
     }
